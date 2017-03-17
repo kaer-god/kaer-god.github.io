b27560469d39a897cee5f3d65161c0f4fb8cb6bd
shen <15376750910@163.com>
2017-03-17 15:00:42 +0800

    ss

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -26,8 +26,8 @@ module.exports = merge(baseWebpackConfig, {
     new webpack.NoErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
+      filename: '_index.html',
+      template: '_index.html',
       inject: true
     })
   ]
